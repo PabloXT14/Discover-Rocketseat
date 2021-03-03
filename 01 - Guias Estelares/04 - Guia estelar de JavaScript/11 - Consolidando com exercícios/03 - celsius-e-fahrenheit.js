7391181f18efb0afd3c9e46a9f90190d5fc9cd17
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-03-03 15:59:43 -0300

    new alteration

---
+++ 01 - Guias Estelares/04 - Guia estelar de JavaScript/11 - Consolidando com exercícios/03 - celsius-e-fahrenheit.js
@@ -0,0 +1,46 @@
+/* ### Celsius em Fahrenheit
+
+- Crie uma função que receba uma string em celsius ou fahrenheit e faça a transformação de uma unidade para outra
+
+C = (F - 32) * 5/9
+
+F = C * 9/5 + 32
+
+*/
+
+function transformTemperature(temperature) {
+    const celsiusExist = temperature.toUpperCase().includes("°C")
+    const fahrenheitExist = temperature.toUpperCase().includes("°F")
+
+
+    // Fluxo de erro
+    if(!celsiusExist && !fahrenheitExist) {
+        throw new Error('Grau não identificado!')
+    }
+
+
+    // Fluxo ideal C -> F
+    let newTemp = Number(temperature.toUpperCase().replace("°C", ""))
+    let formula = (celsius) => celsius * 9/5 + 32
+    let signal = '°F'
+
+
+    // Fluxo alternativo F -> C
+    if(fahrenheitExist) {
+        newTemp = Number(temperature.toUpperCase().replace("°F", ""))
+        formula = (fahrenheit) => (fahrenheit - 32) * 5/9
+        signal = '°C'
+    }
+
+    return formula(newTemp) + signal
+
+}
+
+try {
+
+    console.log(transformTemperature('50°f'))
+
+}catch(error) {
+    // console.log(error)//mostra mais detalhes sobre o erro
+    console.log(error.message)
+}
