8109fab00a63d7212a124fb8002a477357e5068a
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-03-05 14:58:30 -0300

    new alteration

--- Maratona Discover/script.js
+++ Maratona Discover/script.js
@@ -11,27 +11,26 @@ const Modal = {
     }
 }
 
+/* Salvando dados no Local Storage do browser */
+const Storage = {
+    // Pegando dados no localStorage
+    get() {
+        // Convertendo string para o formato array
+        return JSON.parse(localStorage.getItem("dev.finances: transactions")) || []
+    },
 
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021',
-    },
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021',
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021',
-    },
-]
+    // Enviando os dados para o localStorage
+    set(transactions) {
+        // Parêmetros ("nome da chave", "valor(o localStorage sempre salva como string) ")
+        localStorage.setItem("dev.finances: transactions", JSON.stringify(transactions))
+        /*
+            Semelhante a um objeto (o localStorage é um objeto):
+            objeto {
+                chave: valor
+            }
+        */
+    }
+}
 
 
 /*
@@ -41,10 +40,36 @@ const transactions = [
 */
 
 const Transaction = {
-    all: transactions,
+    all: Storage.get(),
+    
+    /* Modo antigo se inserir dados na table
+     [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021',
+        },
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '23/01/2021',
+        },
+        {
+            description: 'Internet',
+            amount: -20000,
+            date: '23/01/2021',
+        },
+    ]
+    */
 
     add(transaction) {
         Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
 
         App.reload()
     },
@@ -84,18 +109,19 @@ const Transaction = {
 }
 
 /* Substituir os dados do HTML com os dados do JS */ 
-
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
+
 
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclasses = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount);
@@ -106,7 +132,7 @@ const DOM = {
             <td class="${CSSclasses}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
         </tr>
         `
@@ -140,48 +166,120 @@ const Utils = {
         })
 
         return signal + value
+    },
+
+    formatAmount(value) {
+        value = Number(value) * 100 // formato correto para a array guardar 
+
+        return value
+    },
+
+    formatDate(date) {
+        const splitDate = date.split("-")
+
+        return `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`
+    },
+}
+
+/* Capturando dados do formulário */
+const Form = {
+    description: document.querySelector("input#description"),
+    amount: document.querySelector("input#amount"),
+    date: document.querySelector("input#date"),
+
+    getValue() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value,
+        }
+    },
+
+    validateFields() {
+        const { description, amount, date } = Form.getValue()
+
+        // verificando se não esta vazio (o trim() remove todos os espaços em branco)
+        if( description.trim() === "" ||
+            amount.trim() === "" ||
+            date.trim() === "" ) {
+                throw new Error("Por favor, preencha todos os campos")
+            }
+    },
+
+    formatValues() {
+        let { description, amount, date } = Form.getValue()
+
+        amount = Utils.formatAmount(amount)
+        date = Utils.formatDate(date)
+
+        return {
+            description, 
+            amount,
+            date,
+        }
+    },
+
+    clearFields() {
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
+    submit(event) {
+        event.preventDefault()// para não aparecer os dados na url
+
+        try {
+
+            // verificar se todas as informações foram preenchidas
+            Form.validateFields()
+
+            // formatar os dados para salvar
+            const transaction =  Form.formatValues()
+
+            // salvar
+            Transaction.add(transaction)
+
+            // apagar os dados do formulário
+            Form.clearFields()
+
+            // modal fechar
+            Modal.close()
+
+            // Atualizar nossa aplicação (opicional pois ja foi feito reload antes ao adicionar a transação com Transaction.add())
+            // App.reload()
+
+
+        } catch(error) {
+            alert(error.message)
+        }
+        
     }
 }
 
 
+
 const App = {
+    // Incio da aplicação
     init() {
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index)
         })
         
         DOM.updateBalance()
+
+        Storage.set(Transaction.all) // Salvando dados no localStorage
     },
+
+    // Recarregando a aplicação (para remover ou adicionar dados)
     reload() {
         DOM.clearTransactions()
         App.init()
     }
 }
 
-
 App.init()
 
-//Novas transações
 
-Transaction.add({
-    id: 4,
-    description: 'Água',
-    amount: -120,
-    date: '23/01/2021'
-})
 
-Transaction.add({
-    id: 5,
-    description: 'Viagem',
-    amount: -13000,
-    date: '23/01/2021'
-})
-
-Transaction.add({
-    id: 6,
-    description: 'Vendas',
-    amount: 200000,
-    date: '24/01/2021'
-})
 
 
