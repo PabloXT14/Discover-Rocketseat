69b8f2655562c12ebd5ed228aebde5592228fea6
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-03-12 16:04:25 -0300

    new alteration

---
+++ 02 - Preparação de Astronautas/04 - Pilotando com a DOM/08 - Praticando/modal.js
@@ -0,0 +1,30 @@
+const modal = document.querySelector('.modal-wrapper')
+const button = document.querySelector('button')
+
+// Funções desativar e ativar modal
+function createModal() {
+    modal.classList.remove('invisible')
+}
+function removeModal() {
+    modal.classList.add('invisible')
+}
+
+
+button.addEventListener('click', createModal)
+
+document.onkeydown = function(event) {
+    const isEscKey = event.key === 'Escape'
+    if(isEscKey) {
+        removeModal()
+        
+    }
+    console.log(event.key)
+}
+
+/* Outra modo para criar o modal na tela */
+// document.addEventListener('keydown', function(event) {
+//     const teclaEnter = event.key === 'Enter'
+//     if(teclaEnter) {
+//         createModal()
+//     }
+// })
