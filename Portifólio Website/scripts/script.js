52925add122728d803e8818216553e71c100c6ef
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-04-20 15:46:45 -0300

    new alteration

--- Portifólio Website/scripts/script.js
+++ Portifólio Website/scripts/script.js
@@ -1,21 +1,31 @@
 // Quando o document(html) estiver pronto execute uma função
-function ready(callback) {
-    // In case the document is already rendered
-    if(document.readyState === "complete") callback()
-    else if(document.addEventListener) document.addEventListener('DOMContentLoaded', callback)
-}
+// function ready(callback) {
+//     // In case the document is already rendered
+//     if(document.readyState === "complete") callback()
+//     else if(document.addEventListener) document.addEventListener('DOMContentLoaded', callback)
+// }
 
-ready(function() {
-    // Executando uma função para verificar certo ponto do scroll da janela/viewport
-    if (window.scrollY > 20) {
-        document.querySelector(".navbar").classList.add("sticky")
-        console.log("OI")
-    } else {
-        document.querySelector(".navbar").classList.remove("sticky")
-    }
+// ready(function() {
+//     // Executando uma função para verificar certo ponto do scroll da janela/viewport
+//     if (window.scrollY > 20) {
+//         document.querySelector(".navbar").classList.add("sticky")
+//         console.log("OI")
+//     } else {
+//         document.querySelector(".navbar").classList.remove("sticky")
+//     }
+// })
+
+
+/* ===== JQuery ===== */
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky")
+        } else {
+            $('.navbar').removeClass("sticky")
+        }
+    })
 })
-
-
 
 
 
