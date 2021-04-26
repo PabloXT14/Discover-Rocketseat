891881998b3b0bac87dde6d57fb73a82da373ef1
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-04-26 13:47:43 -0300

    new alteration

--- 03 - Viajando ao espaço/01 - NodeJS - O motor da nave/6 - Timers/02 - setTimeout.js
+++ 03 - Viajando ao espaço/01 - NodeJS - O motor da nave/6 - Timers/02 - setTimeout.js
@@ -2,21 +2,6 @@
 const timeOut = 3000
 const finished = () => console.log(`done!`)
 
-// setTimeout(finished, timeOut)
+setTimeout(finished, timeOut)
 console.log(`Mostrar`)// aparece antes da fução finished, este é um ex. de assincronismo
-
-const functionCallBack = (message) => {
-    console.log(message)
-}
-
-const Messages = [
-    "Olá, tudo bem!",
-    "Bom Dia",
-    "Boa Tarde",
-    "Boa Noite!"
-]
-
-functionCallBack(Messages[0])
-// setTimeout(functionCallBack(Messages[0]), timeOut)
-
 
--- 03 - Viajando ao espaço/01 - NodeJS - O motor da nave/6 - Timers/04 - setInterval.js
+++ 03 - Viajando ao espaço/01 - NodeJS - O motor da nave/6 - Timers/04 - setInterval.js
@@ -3,3 +3,25 @@ const timeOut = 2000
 const checking = () => console.log(`checked!`)
 
 setInterval(checking, timeOut)
+
+
+/* ============ Outros Testes ========== */
+
+// const Messages = [
+//     "1",
+//     "2",
+//     "3",
+//     "4"
+// ]
+
+// let message = 0
+
+// const fc = () => {
+//      console.log(Messages[message])
+//      message++
+//      if(message === Messages.length) {
+//          clearInterval(interval)
+//      }
+// }
+
+// const interval = setInterval(fc, 2000)
