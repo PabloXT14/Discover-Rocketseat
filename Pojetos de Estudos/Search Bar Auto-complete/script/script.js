cb9b0b57328caa3158ccc0bdbb637662bcb15a34
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-05-08 14:06:44 -0300

    new alteration

---
+++ Pojetos de Estudos/Search Bar Auto-complete/script/script.js
@@ -0,0 +1,18 @@
+// Getting all required elements
+const searchWrapper = document.querySelector(".search-input")
+const inputBox = searchWrapper.querySelector("input")
+const suggBox = searchWrapper.querySelector(".autocom-box")
+
+// If user press any key and release
+inputBox.onkeyup = (e) => {
+    // console.log(e.target.value) // pegando é digitado pelo teclado
+    let userData = e.target.value;// user enetered data
+    let emptyArray = []
+    if(userData) {
+        emptyArray = suggestion.filter( (data) => {
+            return data.toLocaleLowerCase()
+            
+        })
+        console.log(emptyArray)
+    }
+}
---
+++ Pojetos de Estudos/Search Bar Auto-complete/script/suggestions.js
@@ -0,0 +1,11 @@
+let suggestion = [
+    "Login Form in HTML & CSS",
+    "How Learn HTML & CSS",
+    "How to learn JavaScript",
+    "How to became Freelancer",
+    "How to became Web Designer",
+    "YouTube",
+    "YouTuber",
+    "Facebook",
+    "Twitter"
+]
