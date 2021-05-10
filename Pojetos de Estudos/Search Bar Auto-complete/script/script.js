25fefa4ecc98db0586656713303b12f52b6e012d
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-05-10 17:30:35 -0300

    new alteration

--- Pojetos de Estudos/Search Bar Auto-complete/script/script.js
+++ Pojetos de Estudos/Search Bar Auto-complete/script/script.js
@@ -1,37 +1,50 @@
 // Getting all required elements
-const searchWrapper = document.querySelector(".search-input")
-const inputBox = searchWrapper.querySelector("input")
-const suggBox = searchWrapper.querySelector(".autocom-box")
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
 
 // If user press any key and release
 inputBox.onkeyup = (e) => {
     // console.log(e.target.value) // pegando é digitado pelo teclado
     let userData = e.target.value;// user enetered data (dados inseridos pelo usuário)
-    let emptyArray = []
+    let emptyArray = [];
     if(userData) {
         emptyArray = suggestion.filter( (data) => {
             // filtering array value and user char to lowercase and return only those word/sentc which are starts with user entered word
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase())
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
             
-        })
+        });
         emptyArray = emptyArray.map((data) => {
-            return data = '<li>'+ data +'</li>'
-        })
-        console.log(emptyArray)
-        searchWrapper.classList.add("active")// ativando caixa de sugestão caso o usuário digite algo
+            return data = '<li>'+ data +'</li>';
+        });
+        console.log(emptyArray);
+        searchWrapper.classList.add("active");// ativando caixa de sugestão caso o usuário digite algo
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for(let i=0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
     } else {
-
+        searchWrapper.classList.remove("active");// hide autocomplete box
     }
-    showSuggestions(emptyArray);
+    
 }
 
 
-function showSuggestions(list) {
-    let listData
-    if(!list.lenght) {
+function select(element) {
+    let selectUserData = element.textContent;
+    inputBox.value = selectUserData;// passing the user selected list item data in textfield
+    searchWrapper.classList.remove("active");
+}
 
+function showSuggestions(list) {
+    let listData;
+    if(!list.length) {
+        userValue = inputBox.value;
+        listData = '<li>'+ userValue + '</li>';
     } else {
-        listData = list.join('')
+        listData = list.join('');
     }
     suggBox.innerHTML = listData
 }
