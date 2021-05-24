547b361291ece4692c537c165b7152c17e47e321
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-05-24 15:04:51 -0300

    New alteration on Project Studies

---
+++ Pojetos de Estudos/Quiz App with Timer/scripts/questions.js
@@ -0,0 +1,86 @@
+// Creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Canscading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Canscading Style Sheet",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language",
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXtra Multi-Program Language",
+            "eXamine Multiple Language",
+        ]
+    },
+
+];
+
+/*
+What does CSS stand for?
+Common Style Sheet
+Colorful Style Sheet
+Computer Style Sheet
+Canscading Style Sheet
+
+What does PHP stand for?
+Hypertext Preprocessor
+Hypertext Programming
+Hypertext Preprogramming
+Hometext Preprocessor
+
+What does SQL stand for?
+Stylish Question Language
+Stylesheet Query Language
+Statement Question Language
+Structured Query Language
+
+What does XML stand for?
+eXtensible Markup Language
+eXecutable Multiple Language
+eXtra Multi-Program Language
+eXamine Multiple Language
+
+*/
--- Pojetos de Estudos/Quiz App with Timer/scripts/script.js
+++ Pojetos de Estudos/Quiz App with Timer/scripts/script.js
@@ -19,4 +19,45 @@ exit_btn.onclick = ()=> {
 continue_btn.onclick = ()=> {
     info_box.classList.remove("activeInfo") // hide the info box
     quiz_box.classList.add("activeQuiz")    // show the quiz box
+    showQuestions(0);
 }
+
+let que_count = 0
+
+const next_btn = quiz_box.querySelector(".next_btn")
+
+// If Next Button Clicked
+next_btn.onclick = ()=> {
+    if(que_count < questions.length -1){
+        que_count++;
+        showQuestions(que_count)
+    } else {
+        console.log("Questions Completed")
+    }
+
+}
+
+// Getting questions and options from array
+function showQuestions(index) {
+    const que_text = document.querySelector(".que_text")
+    const option_list = document.querySelector(".option_list")
+
+    let que_tag = `<span>${questions[index].numb}. ${questions[index].question}</span>`
+    let option_tag = `
+    <div class="option">
+        <span>${questions[index].options[0]}</span>
+    </div>
+    <div class="option">
+        <span>${questions[index].options[1]}</span>
+    </div>
+    <div class="option">
+        <span>${questions[index].options[2]}</span>
+    </div>
+    <div class="option">
+        <span>${questions[index].options[3]}</span>
+    </div>`
+
+    que_text.innerHTML = que_tag
+    option_list.innerHTML = option_tag
+}
+
