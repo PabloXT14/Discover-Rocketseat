b6a322957e3aa3075e7603a4cf77c890872c85ef
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-05-26 15:20:07 -0300

    update projeto de estudos

--- Projetos de Estudos/Quiz App with Timer/scripts/script.js
+++ Projetos de Estudos/Quiz App with Timer/scripts/script.js
@@ -5,6 +5,9 @@ const exit_btn = info_box.querySelector(".buttons .quit")
 const continue_btn = info_box.querySelector(".buttons .restart")
 const quiz_box = document.querySelector(".quiz_box")
 
+const option_list = document.querySelector(".option_list")
+
+
 // If Start Quiz Button Clicked
 start_btn.onclick = ()=> {
     info_box.classList.add("activeInfo") //show the info box
@@ -44,7 +47,6 @@ next_btn.onclick = ()=> {
 // Getting questions and options from array
 function showQuestions(index) {
     const que_text = document.querySelector(".que_text")
-    const option_list = document.querySelector(".option_list")
 
     let que_tag = `<span>${questions[index].numb}. ${questions[index].question}</span>`
     let option_tag = `
@@ -71,16 +73,38 @@ function showQuestions(index) {
     }
 }
 
+// Icons of correct and incorrect options
+let tickIcon = `<div class="icon tick"><i class="fas fa-check"></i></div>` 
+let crossIcon = `<div class="icon cross"><i class="fas fa-times"></i></div>`
+
 // Function check selected option
 function optionSelected(answer) {
     let userAns = answer.querySelector("span").textContent
     let correctAns = questions[que_count].answer
+    let allOptions = option_list.children.length
+
     if(userAns == correctAns) {
         answer.classList.add("correct")
         console.log("Answer is correct")
+        answer.insertAdjacentHTML("beforeend", tickIcon)
+        // answer.innerHTML += tickIcon // outra forma de fazer
     } else {
         answer.classList.add("incorrect")
         console.log("Answer is wrong")
+        answer.insertAdjacentHTML("beforeend", crossIcon)
+        // if answers is incorrect then automatically selected the correct answer
+        for(let i=0; i < allOptions; i++) {
+            if(option_list.children[i].querySelector("span").textContent == correctAns) {
+                option_list.children[i].setAttribute("class", "option correct")
+                // option_list.children[i].classList.add("correct") // outra forma de fazer
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIcon)
+            }
+        }
+    }
+
+    // once user selected disabled all options
+    for(let i=0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled")
     }
 }
 
