e5e3fb6feb61c50a06b782e91b0906325176c1dc
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-07-04 14:43:36 -0300

    Almost finished Android Challeng Project

--- Projetos de Estudos/Desafio Android/scripts/script.js
+++ Projetos de Estudos/Desafio Android/scripts/script.js
@@ -1,9 +1,9 @@
 /* Select all required elements */
-const iconMenu = document.querySelector("nav ul li.btn")
+const iconMenu = document.querySelector(".btn i")
 const menu = document.querySelector("div.items")
 
 iconMenu.addEventListener('click', function() {
-    iconMenu.firstElementChild.classList.toggle('show')
+    iconMenu.classList.toggle('show')
     menu.classList.toggle('show')
 });
 
