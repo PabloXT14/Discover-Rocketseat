afe1431e04a06675cce42906ce704d68176e83b7
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-08-28 13:47:16 -0300

    Contining study React

--- 05 - React/03 - Create React App/meu-app/src/Header.js
+++ 05 - React/03 - Create React App/meu-app/src/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-function Banana(props) {
+function HeaderComponent(props) {
     return (
         <header>
         <h1 className="title">{props.name}</h1>
@@ -9,14 +9,12 @@ function Banana(props) {
             
             <li>{props.links[0]}</li>
             <li>{props.links[1]}</li>
-            <li>Services</li>
-            <li>Skills</li>
-            <li>Contact</li> 
+            <li>{props.links[2]}</li>
+            <li>{props.links[3]}</li> 
             
-
         </ul>
     </header>
     );
 }
 
-export default Banana;
+export default HeaderComponent;
---
+++ 05 - React/Intro.md
@@ -0,0 +1,10 @@
+# Reac Introdução
+
+## O que é React
+- O **React** é uma biblioteca JavaScript criada pelo Facebook. Sendo uma ferramenta usada para construir componentes de IU(Interface de Usuário).
+
+## Como o React Funciona?
+- _O React cria um DOM VIRTUAL na memória_:
+    Em vez de manipular o DOM do navegador diretamente, o React cria um DOM VIRTUAL na memória, onde faz toda a manipulação necessária, antes de fazer as alterações no DOM do navegador.
+- _React apenas muda o que precisa ser mudado_:
+    O React descobre quais mudanças foram feitas e muda apenas o que precisa ser mudado.
