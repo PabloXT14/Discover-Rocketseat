e62dc4a1bbbb19267bc31eee09c426c13b20f56a
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-09-17 14:57:20 -0300

    Studie about React JS Hook Effects

--- 05 - React/03 - Create React App/meu-app/src/App.js
+++ 05 - React/03 - Create React App/meu-app/src/App.js
@@ -2,8 +2,48 @@ import logo from './logo.svg';
 import Header from "./Header";
 import Counter from "./Counter";
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [show, setShow] = useState(true);
+
+  let time = 0;
+
+  // useEffect(()=> {
+  //   let timer = setInterval(()=> {
+  //     time++;
+  //     console.log(time)
+  //     if(time > 5) {
+  //       clearInterval(timer);
+  //       setShow(false)
+  //     }
+  //   }, 1000)
+  // }, [])
+
+
+  //===== Test useEffect == componentWillUnmount ====
+  // if(show) {
+  //   return (
+  //     <div>
+  //       <Header name="PabloXT" links={["Home", "About", "Services", "Contact"]}></Header>
+  //       <Counter count={5}></Counter>
+  //     </div>
+      
+  //   );
+  // } else {
+  //   return (
+  //     <div>
+  //       <Header name="PabloXT" links={["Home", "About", "Services", "Contact"]}></Header>
+  //       <div className="counter">
+  //           <h1>Counter Excluido</h1>
+  //       </div>
+  //     </div>
+      
+  //   );
+  // }
+
+  //===== Retorno Padrão =====
   return (
     //Obs: colocamos os componentes dentro de uma div pois não dá para retornar mais de um componente JSX por vez
     <div>
--- 05 - React/03 - Create React App/meu-app/src/Counter.js
+++ 05 - React/03 - Create React App/meu-app/src/Counter.js
@@ -1,75 +1,84 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 /* ========== Componente em Class ========== */
-// class CounterComponent extends React.Component {
+//  class CounterComponent extends React.Component {
 
-//     constructor(props) {
-//         super(props);
-//         /* ========== State(Estado) ==========
-//         - State: elemento do React que serve para renderizar novamente a tela todas vez que precisar mudar o estado de uma elemento da página
-//         - Sendo assim, toda vez que precisarmos mudar/alterar um elemento na tela teremos que alterar o <state> através do React
-//         */
-//         this.state = {count: this.props.count}
+//      constructor(props) {
+//          super(props);
+//          /* ========== State(Estado) ==========
+//          - State: elemento do React que serve para renderizar novamente a tela todas vez que precisar mudar o estado de uma elemento da página
+//          - Sendo assim, toda vez que precisarmos mudar/alterar um elemento na tela teremos que alterar o <state> através do React
+//          */
+//          this.state = {count: this.props.count}
 
-//         //Fazendo com que o <this> do método add() seja o mesmo <this> da classe.
-//         this.add = this.add.bind(this);
-//         this.less = this.less.bind(this);
+//          //Fazendo com que o <this> do método add() seja o mesmo <this> da classe.
+//          this.add = this.add.bind(this);
+//          this.less = this.less.bind(this);
+//      }
+
+//        /* ===== Método de Ciclo de Vida ===== */ 
+//      shouldComponentUpdate() {
+//      /*shouldComponentUpdate(): função React, que serve para determinar se o Component deve ou não atualizar a tela (com render) caso ocorra uma alteração de estado<state>
+//          - return true: atualiza a tela
+//          - return false: não atualiza a tela
+//      */
+//          return true;
+
+//      }
+
+//      componentDidMount() {
+//      /*
+//          O método componentDidMount() é executado apenas uma vez, após a saída do componente ter sido renderizada para o DOM (ou seja, é executado depois que a tela já foi renderizada pelo <render>). Este é um bom lugar para configurar um cronômetro ou setar o valor inicial de um elemento:
+//      */
+//          this.setState(JSON.parse(localStorage.getItem("state")))
+//      }
+
+
+//     componentDidUpdate() {
+//      /*
+//         O método componentDidUpdate() é utilizado para quando formos atualizar o valor de um elemento depois que a tela já foi carregada, ex: atualizar o valor do cronômetro
+//      */
 //     }
 
-       /* ===== Método de Ciclo de Vida ===== */ 
-//     shouldComponentUpdate() {
-//     /*shouldComponentUpdate(): função React, que serve para determinar se o Component deve ou não atualizar a tela (com render) caso ocorra uma alteração de estado<state>
-//         - return true: atualiza a tela
-//         - return false: não atualiza a tela
-//     */
-//         return true;
 
-//     }
+//      componentWillUnmount() {
+//      /*
+//          o método componentWillUnmount() é executado toda vez que um elemento vai ser excluido da tela, ou seja, toda vez que vc for excluir um elemento da tela utilize este método (ex: limpar um item de uma todo list, parar um cronômetro)
+//      */
+//  console.log("Counter foi excluido!")
+//      }
 
-//     componentDidMount() {
-//     /*
-//         O método componentDidMount() é executado após a saída do componente ter sido renderizada para o DOM (ou seja, é executado depois que a tela já foi renderizada pelo <render>). Este é um bom lugar para configurar um cronômetro:
-//     */
-//         this.setState(JSON.parse(localStorage.getItem("state")))
-//     }
+//      add() {
+//          //Alterando State: toda vez que for alterar o estado fora do construtor, é preciso utilizar o this.setState()
 
-//     componentWillUnmount() {
-//     /*
-//         o método componentWillUnmount() é executado toda vez que um elemento vai ser excluido da tela, ou seja, toda vez que vc for excluir um elemento da tela utilize este método (ex: limpar um item de uma todo list, parar um cronômetro)
-//     */
-//     }
+//          //Sintaxe que pode dar erro
+//          //this.setState({count: this.state.count + 1})
 
-//     add() {
-//         //Alterando State: toda vez que for alterar o estado fora do construtor, é preciso utilizar o this.setState()
+//          //Sintaxe mais segura
+//          this.setState((state)=> { return {count: state.count + 1}}, ()=> {
+//              console.log(this.state)
+//              localStorage.setItem("state", JSON.stringify(this.state))//armazenando valor do <state> no localStorage
+//          })
+//      }
 
-//         //Sintaxe que pode dar erro
-//         //this.setState({count: this.state.count + 1})
+//      less() {
+//          this.setState((state)=> { return {count: state.count - 1}}, ()=> {
+//              localStorage.setItem("state", JSON.stringify(this.state))
+//          })
+//      }
 
-//         //Sintaxe mais segura
-//         this.setState((state)=> { return {count: state.count + 1}}, ()=> {
-//             console.log(this.state)
-//             localStorage.setItem("state", JSON.stringify(this.state))//armazenando valor do <state> no localStorage
-//         })
-//     }
-
-//     less() {
-//         this.setState((state)=> { return {count: state.count - 1}}, ()=> {
-//             localStorage.setItem("state", JSON.stringify(this.state))
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Counter {this.state.count}</h1>
-//                 <button onClick={this.add}>Add</button>
-//                 <button onClick={this.less}>Less</button>
-//             </div>
+//      render() {
+//          return (
+//              <div className="counter">
+//                  <h1>Counter <span>{this.state.count}</span></h1>
+//                  <button onClick={this.add}>Add</button>
+//                  <button onClick={this.less}>Less</button>
+//              </div>
             
-//         );
-//     }
-// }
+//          );
+//      }
+//  }
 
 
 
@@ -84,7 +93,39 @@ function CounterComponent(props) {
     - É parecido com <this.setState> em uma classe, exceto que não mescla o antigo state com o novo.
     */
 
+
     const [count, setCount] = useState(props.count)//o "count" e o "setCount", pode ser o nome q vc quiser
+
+
+    /* ===== Hooks Effect ===== */
+    /*
+    - o Hooks Effect vão funcionar para controlar o ciclo de vida de um component/elemento (semelhante aos métodos de ciclo de vida em uma classe) nas funções.
+    - Para controlar o ciclo de vida do elemento com Hook Effect utiliza-se a variável global <useEffect>.
+    - Sintaxe: useEffect(action, stateVerify):
+        * action: ação/função que o useEffect vai realizar.
+            OBS: utilize aqui um return() quando quiser que um component suma da tela.
+        * stateVerify: estado/variável do estado que o useEffect vai verificar em tempo real para alterar o valor
+            OBS: use [] para o useEffect verificar somento uma vez/setar o valor inicial de um state (igual ao componentDidMount), mas lembre-se de declarar antes o useEffect == componentDidUpdate .
+    */
+
+    //useEffect == componentDidMount (sentando valor inicial com useEffect)
+    useEffect(()=> {
+        setCount(JSON.parse(localStorage.getItem("count")))
+    }, [])
+
+    //useEffect == componentDidUpdate (setando/atualizando valor de um state)
+    useEffect(()=>{
+        localStorage.setItem("count", count);
+    }, [count])
+
+    //useEffect == componentWillUnmount (excluindo um elemento da tela)
+    useEffect(()=> {
+        //Este return só é executado quando o component <Counter> some da tela
+        return ()=> {
+            console.log("Contador Excluido!!!")
+        }
+
+    }, [])
 
     function add() {
         console.log("update value")
