91437c55feda45408f80a7c87080a31d322146bb
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-09-30 16:18:58 -0300

    Configurated library Babel

---
+++ 05 - React/Ignite ReactJS/Chapter I/01-github-explorer/dist/bundle.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+/* DEMONSTRAÇÃO */
+// const user = {
+//     name: 'Pablo'
+// }
+// console.log(user.address?.street);//nulled colesing operator (nova funcionalidade do ESC6, evita um erro caso alguma propriedade de um objeto não exista)
+
+/* CRIANDO COMPONENTE */
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hellow World!!!");
+}
---
+++ 05 - React/Ignite ReactJS/Chapter I/01-github-explorer/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/* DEMONSTRAÇÃO */
+// const user = {
+//     name: 'Pablo'
+// }
+
+// console.log(user.address?.street);//nulled colesing operator (nova funcionalidade do ESC6, evita um erro caso alguma propriedade de um objeto não exista)
+
+
+/* CRIANDO COMPONENTE */
+function App() {
+    return <h1>Hellow World!!!</h1>;
+}
