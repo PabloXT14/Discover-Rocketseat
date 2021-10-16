174a302d636ec91a7fcc339c7ea2e376d23b7356
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-10-16 13:00:47 -0300

    Finished Environment Config React JS

--- 05 - React/Ignite ReactJS/Chapter I/01-github-explorer/src/App.jsx
+++ 05 - React/Ignite ReactJS/Chapter I/01-github-explorer/src/App.jsx
@@ -1,4 +1,6 @@
 import './styles/global.css';
+import './styles/sass-style.scss';
+
 
 export function App() {
 
@@ -8,8 +10,8 @@ export function App() {
 
     return (<div>
         <h2>Hellow World!</h2>
-        {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aliquid cum consequuntur itaque vero non dignissimos et accusamus nam ut maxime, assumenda sed impedit esse explicabo officiis quas, quae aspernatur?</p>
-        <button>Click</button> */}
+        <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aliquid cum consequuntur itaque vero non dignissimos et accusamus nam ut maxime, assumenda sed impedit esse explicabo officiis quas, quae aspernatur?</p>
+        <button>Click</button>
     </div>
     );
 }
--- 05 - React/Ignite ReactJS/Chapter I/01-github-explorer/webpack.config.js
+++ 05 - React/Ignite ReactJS/Chapter I/01-github-explorer/webpack.config.js
@@ -38,6 +38,11 @@ module.exports = {
                 test: /\.css$/,
                 exclude: /node_modules/,
                 use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader','css-loader','sass-loader'],
             }
         ],
     }
