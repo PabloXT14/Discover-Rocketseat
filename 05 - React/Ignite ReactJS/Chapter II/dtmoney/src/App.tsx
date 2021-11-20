ad7fea5ee14da16b0a74f5ecdcd2d86ee8c2bbd0
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-11-20 14:21:44 -0300

    More about ReactJS

--- 05 - React/Ignite ReactJS/Chapter II/dtmoney/src/App.tsx
+++ 05 - React/Ignite ReactJS/Chapter II/dtmoney/src/App.tsx
@@ -1,22 +1,24 @@
 import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
+// const Title = styled.h1`
+//   font-size: 64px;
+//   color: #8257e6;
 
-  button {
-    color: dodgerblue;
-  }
-`
+//   button {
+//     color: dodgerblue;
+//   }
+// `
 
 export function App() {
   return (
     <div className="App">
-      <Title>
-        Hellow Worold
-        <button>Click Me</button>
-      </Title>
+      {/* Estilização global com Styled Compon */}
+      <GlobalStyle />
+
+      <h1>Hellow World</h1>
+      <input type="text" disabled></input>
     </div>
   );
 }
