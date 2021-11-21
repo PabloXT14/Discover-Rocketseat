fbf5f374ec53e5a2c49dde5ffffe79dc1f96226d
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-11-21 14:34:37 -0300

    Small changes

--- 05 - React/Ignite ReactJS/Chapter II/dtmoney/src/App.tsx
+++ 05 - React/Ignite ReactJS/Chapter II/dtmoney/src/App.tsx
@@ -11,14 +11,36 @@ import { GlobalStyle } from './styles/global';
 //   }
 // `
 
+const ButtonLogin = styled.button`
+    display: block;
+    margin: 20px auto;
+    height: 40px;
+    width: 150px;
+    border: none;
+    outline: none;
+    border-radius: 3px;
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    font-weight: 500;
+    background-color: var(--blue-light);
+    color: #FFF;
+    transition: all 0.3s ease;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+`
+
+
 export function App() {
   return (
     <div className="App">
-      {/* Estilização global com Styled Compon */}
+      {/* Estilização global com Styled Comp. */}
       <GlobalStyle />
 
       <h1>Hellow World</h1>
       <input type="text" disabled></input>
+      <ButtonLogin>Click Me</ButtonLogin>
     </div>
   );
 }
