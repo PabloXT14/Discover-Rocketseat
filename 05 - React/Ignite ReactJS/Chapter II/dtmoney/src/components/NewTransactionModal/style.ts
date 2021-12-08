fa85154e8dd00cc9682fd3445f9010d980b41f99
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-12-08 16:38:18 -0300

    More about ReactJS, NodeJS

--- 05 - React/Ignite ReactJS/Chapter II/dtmoney/src/components/NewTransactionModal/style.ts
+++ 05 - React/Ignite ReactJS/Chapter II/dtmoney/src/components/NewTransactionModal/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -46,4 +47,42 @@ export const Container = styled.form`
             filter: brightness(0.9);
         }
     }
+`
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0.5rem;
+
+    button {
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: transparent;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        transform: border-color 0.2s ease;
+
+        &:hover {
+            border-color: ${darken(0.1, '#d7d7d7')};// 0.1 escurece a cor em 10% 
+        }
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+        }
+    }
+
 `
--- 06 - NodeJS/Chapter I/fundamentos-nodejs/src/index.js
+++ 06 - NodeJS/Chapter I/fundamentos-nodejs/src/index.js
@@ -9,12 +9,36 @@ sintaxe:
 - response: resposta do servidor ao nosso request/pedido
 */
 
-app.get("/", (request, response) => {
-    return (
-        // response.send("Hello World!") // sendo(): response do tipo string
-        response.json({ message: "Hellow World Ignite 🚀🚀🚀" }) // json(): response do tipo array, objecto, string 
-    );
-});
+// app.get("/", (request, response) => {
+//     return (
+//         // response.send("Hello World!") // sendo(): response do tipo string
+//         //response.json({ message: "Hellow World Ignite 🚀🚀🚀" }) // json(): response do tipo array, objecto, string 
+//     );
+// });
+
+
+/* ===== Utilizando os Métodos HTTP ===== */
+app.get("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+})
+
+app.post("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+// para atualizar algo no DB(Banco de Dados) é preciso passar um parametro pela rota (neste caso estaremos passando o id do curso)
+app.put("/courses:id", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 2.1", "Curso 2.2"])
+})
+
+app.patch('/courses/:id', (request, response) => {
+    return response.json(["Curso 1", "Curso 2.0", "Curso 2.1", "Curso 2.2"])
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Curso 1", "Curso 2.0", "Curso 2.1"])
+})
+
 
 app.listen(3333);//start servidor da nossa aplicação na porta 3333 (o qlq porta que vc quiser) 
 // na url aparecerá http:localhost:3333
