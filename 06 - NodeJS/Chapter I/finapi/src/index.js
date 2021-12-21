dbd7890e749347c6cc10bd936dc65f1bb5a291c9
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-12-20 22:13:09 -0300

    More about ReactJS and NodeJS

--- 06 - NodeJS/Chapter I/finapi/src/index.js
+++ 06 - NodeJS/Chapter I/finapi/src/index.js
@@ -18,12 +18,18 @@ const customers = [];//DB fake
 /* Rota para criação de conta */
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();//gerando nosso id randomico
+
+    // Verificando se cpf já existe no DB 
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" })
+    }
 
     customers.push({
-        cpf,
+        id: uuidv4(), //gerando nosso id randomico
         name,
-        id,
+        cpf,
         statement: []
     });
 
@@ -31,4 +37,13 @@ app.post('/account', (request, response) => {
     return response.status(201).json(customers);
 });
 
-app.listen(3333);
+/* Rota para listar extrado de um cliente */
+app.get('/statement/:cpf', (request, response) => {
+    const { cpf } = request.params;//pegando cpf do route params
+
+    const customer = customers.find(customer => Number(customer.cpf) === Number(cpf));//check qual customer tem o cpf informado
+
+    return response.json(customer.statement);//retornando extrato do customer
+});
+
+app.listen(3333, () => console.log("API running on port 3333"));
