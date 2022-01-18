93c07516ee9fcaf0336590c991044aca6611a11a
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-01-18 17:16:43 -0300

    More about NodeJS

---
+++ 06 - NodeJS/Chapter II/Conceitos Typescript/typescript-project/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get("/", (request, response) => {
+    response.json({ message: "Hello World" })
+});
+
+
+app.listen(3333, () => { console.log('Server running on port 3333') })
