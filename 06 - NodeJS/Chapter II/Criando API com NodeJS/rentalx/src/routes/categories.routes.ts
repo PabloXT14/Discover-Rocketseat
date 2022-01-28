43bd0bb489f81d40a5ed200ac194281d25dd7cc0
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-01-28 16:37:50 -0300

    More about NodeJS

--- 06 - NodeJS/Chapter II/Criando API com NodeJS/rentalx/src/routes/categories.routes.ts
+++ 06 - NodeJS/Chapter II/Criando API com NodeJS/rentalx/src/routes/categories.routes.ts
@@ -1,24 +1,23 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    const categorie = {
-        id: uuidV4(),
+    const categorie = new Category();
+
+    Object.assign(categorie, {
         name,
         description,
         created_at: new Date(),
-    };
-
-    categories.push({
-        name,
-        description,
     });
+
+    categories.push(categorie);
 
     // return response.status(201).send('Categorie Created');
     return response.status(201).json(categorie);
