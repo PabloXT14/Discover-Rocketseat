be1ceffdb422685d7924fe84255c43929277fc03
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-02-04 17:16:11 -0300

    More about EJS

--- 03 - Viajando ao espaço/02 - NodeJS + EJS - HTML inteligente/projeto-com-ejs/server.js
+++ 03 - Viajando ao espaço/02 - NodeJS + EJS - HTML inteligente/projeto-com-ejs/server.js
@@ -7,7 +7,14 @@ app.set("view engine", "ejs");
 
 /* ROTAS */
 app.get("/", (req, res) => {
-    res.render('pages/index');
+    const users = [
+        { name: 'John Doe', age: 18, email: 'johndoe2022@gmail.com' },
+        { name: 'Diego Fernandes', age: 25, email: 'diego2022@gmail.com' },
+        { name: 'Mayk Brito', age: 32, email: 'maykbrito2022@gmail.com' }
+    ]
+
+
+    res.render('pages/index', { users: users });
 });
 
 app.get("/about", (req, res) => {
